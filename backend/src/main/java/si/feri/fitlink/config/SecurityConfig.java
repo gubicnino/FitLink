b91a7a7b9bdac4c426d461679aa1099a23125c3d
ninogubic <gubicnino@gmail.com>
2026-06-03 21:09:59 +0200

    Fix: Added deployed frontend to cors

--- backend/src/main/java/si/feri/fitlink/config/SecurityConfig.java
+++ backend/src/main/java/si/feri/fitlink/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173"));
+                List.of("http://localhost:5173", "https://fit-link-alpha.vercel.app"));
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
